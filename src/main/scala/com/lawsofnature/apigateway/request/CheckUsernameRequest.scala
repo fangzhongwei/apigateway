f9e38c3a3406125285bc5a54846b68bb9c44eaa0
fangzhongwei <zhongwei.fang@56qq.com>
2016-12-07 19:04:48 +0800

    interface change

--- src/main/scala/com/lawsofnature/apigateway/request/CheckUsernameRequest.scala
+++ src/main/scala/com/lawsofnature/apigateway/request/CheckUsernameRequest.scala
@@ -6,5 +6,5 @@ import com.lawsofnature.common.exception.ErrorCode._
 /**
   * Created by fangzhongwei on 2016/12/7.
   */
-class CheckUsernameRequest (@Validate(required = true, maxLength = 64, error = EC_INVALID_REQUEST)
+case class CheckUsernameRequest (@Validate(required = true, maxLength = 64, error = EC_INVALID_REQUEST)
                             var un: String)
