3e5f5a2bdfa3ca84e851862a7e9968031e1b876e
fangzhongwei <zhongwei.fang@56qq.com>
2016-12-08 16:44:28 +0800

    logout api

--- src/main/scala/com/lawsofnature/apigateway/action/MemberAction.scala
+++ src/main/scala/com/lawsofnature/apigateway/action/MemberAction.scala
@@ -5,7 +5,7 @@ import javax.inject.Inject
 import RpcMember.MemberResponse
 import com.lawsofnature.apigateway.annotations.{ApiMapping, Param}
 import com.lawsofnature.apigateway.enumerate.ParamSource
-import com.lawsofnature.apigateway.response.ApiResponse
+import com.lawsofnature.apigateway.response.{ApiResponse, Profile}
 import com.lawsofnature.apigateway.service.MemberService
 import com.lawsofnature.common.exception.ErrorCode
 import org.slf4j.{Logger, LoggerFactory}
@@ -25,7 +25,7 @@ class MemberActionImpl @Inject()(memberService: MemberService) extends MemberAct
                           traceId: String): ApiResponse = {
     val memberResponse: MemberResponse = memberService.getMemberByMemberId(traceId, getMemberId)
     memberResponse.success match {
-      case true => ApiResponse.make(data = memberResponse)
+      case true => ApiResponse.make(data = Profile(memberResponse.username, memberResponse.identityList(0).pid, memberResponse.identityList(0).identity))
       case false => ApiResponse.makeErrorResponse(ErrorCode.get(memberResponse.code))
     }
   }
--- src/main/scala/com/lawsofnature/apigateway/response/Member.scala
+++ src/main/scala/com/lawsofnature/apigateway/response/Member.scala
@@ -3,5 +3,4 @@ package com.lawsofnature.apigateway.response
 /**
   * Created by fangzhongwei on 2016/12/3.
   */
-
-class Profile()
+case class Profile(var un:String, var pid:Int, var i:String)
