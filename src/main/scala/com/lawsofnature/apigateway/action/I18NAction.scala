7a8c6180dfb9917fe410ea6fca8c9cbfa4d36dd6
fangzhongwei <zhongwei.fang@56qq.com>
2017-01-13 18:14:51 +0800

    i18n

--- src/main/scala/com/lawsofnature/apigateway/action/I18NAction.scala
+++ src/main/scala/com/lawsofnature/apigateway/action/I18NAction.scala
@@ -5,21 +5,23 @@ import javax.inject.Inject
 import RpcI18N.{PullResourceRequest, ResourceResponse}
 import com.lawsofnature.apigateway.annotations.{ApiMapping, Param}
 import com.lawsofnature.apigateway.domain.http.req.pullresource.PullResourceReq
+import com.lawsofnature.apigateway.domain.http.req.simple.SimpleReq
 import com.lawsofnature.apigateway.domain.http.resp.resource.{Resource, ResourceResp}
 import com.lawsofnature.apigateway.enumerate.ParamSource
 import com.lawsofnature.apigateway.service.I18NService
+import com.lawsofnature.common.exception.ErrorCode
+import org.apache.commons.lang.StringUtils
 
 /**
   * Created by fangzhongwei on 2017/1/13.
   */
 trait I18NAction {
-  def getLatest(traceId: String, lan: String): ResourceResp
+  def getLatest(traceId: String, req: SimpleReq): ResourceResp
 
   def pullLatest(traceId: String, req: PullResourceReq): ResourceResp
 }
 
 class I18NActionImpl @Inject()(i18NService: I18NService) extends I18NAction {
-
   implicit def convert2Proto(response: ResourceResponse): ResourceResp = {
     val resourceList: Array[RpcI18N.Resource] = response.resourceList
     (resourceList != null && !resourceList.isEmpty) match {
@@ -40,12 +42,16 @@ class I18NActionImpl @Inject()(i18NService: I18NService) extends I18NAction {
   @ApiMapping(id = 1006, ignoreSession = true)
   override def getLatest(@Param(required = true, source = ParamSource.HEADER, name = "TI")
                          traceId: String,
-                         @Param(required = true, source = ParamSource.PARAM)
-                         lan: String): ResourceResp = {
-    val resourceResponse: ResourceResponse = i18NService.getLatest(traceId, lan)
-    resourceResponse.code match {
-      case "0" => resourceResponse
-      case _ => ResourceResp(code = resourceResponse.code)
+                         @Param(required = true, source = ParamSource.BODY)
+                         req: SimpleReq): ResourceResp = {
+    val lan: String = req.param0
+    StringUtils.isBlank(lan) match {
+      case true => ResourceResp(code = ErrorCode.EC_INVALID_REQUEST.getCode)
+      case false => val resourceResponse: ResourceResponse = i18NService.getLatest(traceId, lan)
+        resourceResponse.code match {
+          case "0" => resourceResponse
+          case _ => ResourceResp(code = resourceResponse.code)
+        }
     }
   }
 
